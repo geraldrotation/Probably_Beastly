e963743da6d9c991ffcce6aef890b84255ca9340
OpenShift guest <dylancn@hawaii.edu>
2014-01-01 20:52:14 -0500

    First commit

---
+++ functions.lua
@@ -0,0 +1,368 @@
+-----------------------------------------------------------------------------------------------------------------------------
+-- Initialize Tables -------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
+local ts = {} 
+
+ts.queueSpell = nil
+ts.queueTime = 0
+-----------------------------------------------------------------------------------------------------------------------------
+-- Notify Frame -- (c)xrn overlay for pqr -----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+    local function onUpdate(self,elapsed) 
+      if self.time < GetTime() - 2.5 then
+        if self:GetAlpha() == 0 then self:Hide() else self:SetAlpha(self:GetAlpha() - .05) end
+      end
+    end
+    ecn = CreateFrame("Frame",nil,ChatFrame1) 
+    ecn:SetSize(ChatFrame1:GetWidth(),30)
+    ecn:Hide()
+    ecn:SetScript("OnUpdate",onUpdate)
+    ecn:SetPoint("TOPLEFT",0,150)
+    ecn.text = ecn:CreateFontString(nil,"OVERLAY","MovieSubtitleFont")
+    ecn.text:SetAllPoints()
+    ecn.texture = ecn:CreateTexture()
+    ecn.texture:SetAllPoints()
+    ecn.texture:SetTexture(0,0,0,.50) 
+    ecn.time = 0
+    function ecn:message(message) 
+      self.text:SetText(message)
+      self:SetAlpha(1)
+      self.time = GetTime() 
+      self:Show() 
+    end
+    
+    local function onUpdate(self,elapsed) 
+      if self.time < GetTime() - 2.8 then
+        if self:GetAlpha() == 0 then self:Hide() else self:SetAlpha(self:GetAlpha() - .05) end
+      end
+    end
+    ecn = CreateFrame("Frame",nil,ChatFrame1) 
+    ecn:SetSize(ChatFrame1:GetWidth(),30)
+    ecn:Hide()
+    ecn:SetScript("OnUpdate",onUpdate)
+    ecn:SetPoint("TOP",0,0)
+    ecn.text = ecn:CreateFontString(nil,"OVERLAY","MovieSubtitleFont")
+    ecn.text:SetAllPoints()
+    ecn.texture = ecn:CreateTexture()
+    ecn.texture:SetAllPoints()
+    ecn.texture:SetTexture(0,0,0,.50) 
+    ecn.time = 0
+    function ecn:message(message) 
+      self.text:SetText(message)
+      self:SetAlpha(1)
+      self.time = GetTime() 
+      self:Show() 
+    end
+    
+    
+    
+ProbablyEngine.command.register('ts', function(msg, box)
+  local command, text = msg:match("^(%S*)%s*(.-)$")
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- Toggle -------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+  if command == 'pause' then
+    if ProbablyEngine.config.read('button_states', 'MasterToggle', false) then
+        ProbablyEngine.buttons.toggle('MasterToggle')
+        ecn:message("|cFFB30000Beastly DISABLED")
+    else
+        ProbablyEngine.buttons.toggle('MasterToggle')
+        ecn:message("|cFF00B34ABeastly ENABLED")
+    end
+  end
+  if command == 'kick' then
+    if ProbablyEngine.config.read('button_states', 'interrupt', false) then
+      ProbablyEngine.buttons.toggle('interrupt')
+      ecn:message("|cFFB30000Interrupts DISABLED")
+    else
+      ProbablyEngine.buttons.toggle('interrupt')
+      ecn:message("|cFF00B34AInterrupts ENABLED")
+    end
+  end
+
+  if command == 'cooldowns' then
+    if ProbablyEngine.config.read('button_states', 'cooldowns', false) then
+      ProbablyEngine.buttons.toggle('cooldowns')
+      ecn:message("|cFFB30000Offensive Cooldowns DISABLED")
+    else
+      ProbablyEngine.buttons.toggle('cooldowns')
+      ecn:message("|cFF00B34AOffensive Cooldowns ENABLED")
+    end
+  end
+
+  if command == 'aoe' then
+    if ProbablyEngine.config.read('button_states', 'multitarget', false) then
+      ProbablyEngine.buttons.toggle('multitarget')
+      ecn:message("|cFFB30000AoE DISABLED")
+    else
+      ProbablyEngine.buttons.toggle('multitarget')
+      ecn:message("|cFF00B34AAoE ENABLED")
+    end
+  end
+
+  if command == 'aspect' then
+    if ProbablyEngine.config.read('button_states', 'aspect', false) then
+      ProbablyEngine.buttons.toggle('aspect')
+      ecn:message("|cFFB30000Auto Aspect DISABLED")
+    else
+      ProbablyEngine.buttons.toggle('aspect')
+      ecn:message("|cFF00B34AAuto Aspect ENABLED")
+    end
+  end
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- Spell Queue -- thank you merq for basic code -----------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+  if command == "Concussive" or command == 5116 then
+    ts.queueSpell = 5116
+    ecn:message("Concussive Shot queued")
+  elseif command == "Widow" or command == 82654 then
+    ts.queueSpell = 82654
+    ecn:message("Widow Venom queued")
+  elseif command == "Tranquilizing" or command == 19801 then
+    ts.queueSpell = 19801
+    ecn:message("Tranquilizing Shot queued")
+  elseif command == "amoc" or command == 131894 then
+    ts.queueSpell = 131894
+    ecn:message("A Murder of Crows queued")
+  elseif command == "Lynx" or command == 120697 then
+    ts.queueSpell = 120697
+    ecn:message("Lynx Rush queued")
+  elseif command == "Intimidation" or command == 19577 then
+    ts.queueSpell = 19577
+    ecn:message("Intimidation queued") 
+  else
+    ts.queueSpell = nil
+  end
+  if ts.queueSpell ~= nil then ts.queueTime = GetTime() end
+end)
+-----------------------------------------------------------------------------------------------------------------------------
+-- Spell Queue Check -- thank you merq for basic code ----------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
+ts.checkQueue = function (spellId)
+    if (GetTime() - ts.queueTime) > 2 then
+        ts.queueTime = 0
+        ts.queueSpell = nil
+    return false
+    else
+    if ts.queueSpell then
+        if ts.queueSpell == spellId then
+            if ProbablyEngine.parser.lastCast == GetSpellName(spellId) then
+                ts.queueSpell = nil
+                ts.queueTime = 0
+                if spellId == 5116 then
+                    ecn:message("|cFFB30000Concussive Shot casted")
+                elseif spellId == 82654 then
+                    ecn:message("|cFFB30000Widow Venom casted")
+                elseif spellId == 19801 then
+                    ecn:message("|cFFB30000Tranq Shot casted")
+                elseif spellId == 131894 then
+                    ecn:message("|cFFB30000A Murder of Crows casted")
+                elseif spellId == 120697 then
+                    ecn:message("|cFFB30000Lynx rush casted")
+                end
+            end
+        return true
+        end
+    end
+    end
+    return false
+end
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- Check for T16 2Piece------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+--/run local counter=0; local set = GetEquipmentSetInfo(1); local itemArray=GetEquipmentSetItemIDs(set); if itemArray[1] == 99080 then counter = counter + 1; print(counter) end; if itemArray[5] == 99085 then counter = counter + 1; print(counter) end;
+
+function ts.t16()
+    local counter=0
+    
+    if GetEquipmentSetInfo(1) == nil then 
+        return false
+    end
+    
+    local set = GetEquipmentSetInfo(1) 
+    local itemArray=GetEquipmentSetItemIDs(set)
+    
+    if itemArray[1] == 99080 or 99660 or 99157 or 99402 then 
+        counter = counter + 1; 
+    end
+    if itemArray[3] == 99082 or 99574 or 99159 or 99404 then 
+        counter = counter + 1; 
+    end
+    if itemArray[5] == 99085 or 99577 or 99167 or 99405 then 
+        counter = counter + 1; 
+    end
+    if itemArray[7] == 99081 or 99573 or 99158 or 99403 then 
+        counter = counter + 1; 
+    end
+    if itemArray[10] == 99086 or 99578 or 99168 or 99406 then 
+        counter = counter + 1; 
+    end
+    
+    if counter >= 2 then
+        return true
+    end
+end
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- Rabid---------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+
+function ts.rabid()
+    local start, duration, enabled = GetSpellCooldown(53401)
+    
+    if UnitBuff("player", "The Beast Within") then
+        if duration == 0 then
+            return true
+        end
+    end
+end
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- Cobra shot & kill command-------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+
+function ts.casttime(spellId)
+    --Get cast time for spellID
+    local name, rank, icon, cost, isFunnel, powerType, castTime, minRange, maxRange = GetSpellInfo(spellId)
+    --Change castTime from milliseconds to seconds
+    local mstos = ((castTime + 150) / 1000)
+    --Get the Current Time
+    local current_time = GetTime()
+    --Get time that Kill Command was casted at
+    local start, duration, enabled = GetSpellCooldown(34026)
+    --Calculate time elapsed
+    local time_elapsed = (current_time - start)
+    --Calculate Cooldown remaining
+    local cooldown_remaining = (duration - time_elapsed)
+    
+    if duration == 0 then
+        return false
+    end
+    
+    if cooldown_remaining >= mstos then
+        return true
+    end
+end
+
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- Cobra shot doesn't cap us-------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+
+function ts.cobra(spellId)
+    local max_focus = UnitPowerMax("player")
+    local current_focus = UnitPower("player")
+    local regen_focus = GetPowerRegen()
+    local missing_focus = (max_focus - current_focus)
+    local name, rank, icon, cost, isFunnel, powerType, castTime, minRange, maxRange = GetSpellInfo(spellId)
+    local mstos = (castTime / 1000)
+    
+    local ttf = (current_focus + 14) + (mstos*regen_focus)
+    
+    if ttf >= 105 then
+        return false
+    elseif ttf < 105 then
+        return true
+    end
+end
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- GCD doesnt clip kill command----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+
+function ts.clip()
+    --Get the Current Time
+    local current_time = GetTime()
+    --Get time that Kill Command was casted at
+    local start, duration, enabled = GetSpellCooldown(34026)
+    --Calculate time elapsed
+    local time_elapsed = (current_time - start)
+    --Calculate Cooldown remaining
+    local cooldown_remaining = (duration - time_elapsed)
+    --Calculate GCD
+    local haste = UnitSpellHaste("player")
+    local gcd = (1.5 / ((haste/100) + 1)) + 0.1
+    
+    if duration == 0 then
+        return false
+    end
+    
+    if cooldown_remaining >= gcd then
+        return true
+    end
+end
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- Focus Fire----------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+
+function ts.focusfire()
+    --Do we have Bestial Wrath up?
+    if UnitBuff("player", "The Beast Within") then
+        return false
+    end
+    --Check stacks of Frenzy
+    local spell={UnitBuff("player", "Frenzy")} 
+        if UnitBuff("player", "Frenzy") and spell[4]==5 then
+        return true
+    end
+end
+    
+--/run local name, rank, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, shouldConsolidate, spellId = UnitBuff("player", "Aspect of the Iron Hawk"); print(icon)
+-----------------------------------------------------------------------------------------------------------------------------
+-- Time to Max Focus --------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+--[[    
+function ts.focus(value)    
+    local max_focus = UnitPowerMax("player")
+    local current_focus = UnitPower("player")
+    local regen_focus = GetPowerRegen()
+    local missing_focus = (max_focus - current_focus)
+    local ttf = (missing_focus / regen_focus)
+]]
+-----------------------------------------------------------------------------------------------------------------------------
+-- Time to Max Energy -------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------
+
+function ts.energyless(value)
+    local max_energy = UnitPowerMax("player")
+    local current_energy = UnitPower("player")
+    local energy_regen = GetPowerRegen()
+    local energy_missing = (max_energy - current_energy)
+    local ttm = (energy_missing / energy_regen)
+    
+    if ttm <= value then
+        return true
+    end
+end
+
+function ts.energygreater(value)
+    local max_energy = UnitPowerMax("player")
+    local current_energy = UnitPower("player")
+    local energy_regen = GetPowerRegen()
+    local energy_missing = (max_energy - current_energy)
+    local ttm = (energy_missing / energy_regen)
+    
+    if ttm >= value then
+        return true
+    end
+end
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- Healthstone -------------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
+function ts.Healthstone(...)
+    local PlayerHP = 100 * UnitHealth("player") / UnitHealthMax("player")
+    if PlayerHP < 35
+    and GetItemCount(5512,false,false) > 0 
+    and ( select(2, GetItemCooldown(5512)) == 0 ) then
+        return true
+    end
+end
+-----------------------------------------------------------------------------------------------------------------------------
+-- Register Library --------------------------------------------------------------------------------------------------------- 
+-----------------------------------------------------------------------------------------------------------------------------
+ProbablyEngine.library.register("ts", ts)
---
+++ rotation.lua
@@ -0,0 +1,250 @@
+-- ProbablyEngine Rotation Packager
+-- Custom Beast Mastery Hunter Rotation
+-- Created on Dec 31st 2013 4:57 am
+ProbablyEngine.rotation.register_custom(253, "Beastly", {
+
+  --Queue
+    --Concussive
+    { "!5116", "@ts.checkQueue(5116)" },
+    --Widow Venom
+    { "!82654", "@ts.checkQueue(82654)" },
+    --Tranq
+    { "!19801", "@ts.checkQueue(19801)" },
+    --A Murder of Crows
+    { "!131894", "@ts.checkQueue(131894)" },
+    --Lynx Rush
+    { "!120697", "@ts.checkQueue(120697)" },
+    --Intimidation
+    { "!19577", "@ts.checkQueue(19577)" },
+    
+  --Binding Shot
+    { "109248", "modifier.ralt", "ground" },
+  --Wyvern Sting
+    { "19386", "modifier.lalt", "mouseover" },
+    
+  --Buffs
+    --Aspect of the Hawk
+    {{
+        { "13165", "!player.buff(13165)" },
+
+    --Aspect of the Iron Hawk
+        { "109260", "!player.buff(109260)" },
+    }, "toggle.aspect" },
+
+
+  --Revive Pets
+    --Heart of the Phoenix
+    { "55709", { "!pet.exists", "!pet.alive" }},
+    
+    --Revive Pet
+    { "982", "!pet.alive" },
+
+
+  --Self Healing & Survival
+    --Exhilaration
+    { "109304", "player.health <= 45" },
+    
+    --Healthstone
+    { "!/run UseItemByName(5512)", "@ts.Healthstone" },
+    
+    --Mend Pet
+    { "136", { "pet.exists", "pet.health <= 45", "!pet.buff(136)" }},
+    
+    
+  --Misdirection
+    --MD @ focus
+    { "34477", { "!player.buff(34477)", "target.threat >= 50", "focus.exists" }, "focus" },
+    
+    --MD @ Pet
+    { "34477", { "!player.buff(34477)", "target.threat >= 50", "!focus.exists", "pet.exists" }, "pet" },
+    
+    
+  --Interrupts
+    {{
+    --Counter Shot
+        { "147362", "target.interruptAt(75)" },
+    --Masters Call
+        { "53271", "player.state.snare", "player" },
+        { "53271", "player.state.root", "player" },
+    }, "modifier.interrupts" },
+
+
+  --Cooldowns
+    {{
+    --Stampede
+        { "121818" },
+    --A Murder of Crows
+        { "131894" },
+    --Lynx rush
+        { "120697" },
+    --Rapid Fire
+        { "3045" },
+    --Globes
+        { "#gloves" },
+    }, "modifier.cooldowns" },
+
+  --Aoe
+    {{
+    --5+ Targets
+        {{
+        --Bestial Wrath
+            { "19574", { "@coreHunter.petInRange", "pet.exists", "player.spell(34026).cooldown <= 2" }},
+        --Rabid Doesnt work
+            { "!/run CastSpellByID(53401)", { "@coreHunter.petInRange", "@ts.rabid" }},
+        --Fervor
+            { "82726", { "player.focus < 50", "pet.focus < 50" }},
+        --Multishot
+            { "2643" },
+        --Kill Shot
+            { "53351", "target.health <= 20" },
+        --Glaive toss
+            { "117050" },
+        --Barrage
+            { "120360" },
+        --Dire Beast
+            { "120679" },
+        --Cobra Shot
+            { "77767" },
+        }, "modifier.enemies >= 5" }, 
+    
+    --3/4 Targets
+        {{
+        --Bestial Wrath
+            { "19574", { "@coreHunter.petInRange", "pet.exists", "player.spell(34026).cooldown <= 2" }},
+        --Rabid Doesnt work
+            { "!/run CastSpellByID(53401)", { "@coreHunter.petInRange", "@ts.rabid" }},
+        --Fervor
+            { "82726", { "player.focus < 50", "pet.focus < 50" }},
+        --Multishot
+            { "2643", { "!pet.buff(Beast Cleave)", "player.focus >= 75", "player.spell(34026).cooldown <= 2" }},
+        --Kill Command
+            { "34026", { "@coreHunter.petInRange", "pet.exists" }},
+        --Kill Shot
+            { "53351", "target.health <= 20" },
+        --Multishot
+            { "2643", { "player.focus >= 40", "player.spell(34026).cooldown >= 4" }},
+        --Rapid Fire T16 2piece
+            { "3045", { "player.spell(3045).cooldown = 0", "@ts.t16" }},
+        --Serpent Sting
+            { "1978", "!target.debuff(118253)" },
+        --Glaive toss
+            { "117050" },
+        --Dire Beast
+            { "120679" },
+        --Focus Fire
+            { "82692", "@ts.focusfire" },
+        --Cobra Shot refresh Serpent Sting
+            { "77767", "target.debuff(118253).duration <= 3" },
+        --Arcane Shot
+            { "3044", { "player.focus >= 50", "player.spell(34026).cooldown >= 3", "pet.buff(115939" }},
+        --Arcane Shot
+            { "3044", { "player.focus >= 70", "pet.buff(115939)" }},
+        --Cobra Shot
+            { "77767", { "@ts.casttime(77767)", "@ts.cobra(77767)", "!player.spell(34026).cooldown <= 1" }},
+        --Cobra Shot
+            { "77767", { "player.focus < 35", "player.spell(34026).cooldown >= 2" }},
+        }, { "modifier.enemies > 2", "modifier.enemies < 5" }},    
+    
+    --2 Targets
+        {{
+        --Bestial Wrath
+            { "19574", { "@coreHunter.petInRange", "pet.exists", "player.spell(34026).cooldown <= 2" }},
+        --Rabid Doesnt work
+            { "!/run CastSpellByID(53401)", { "@coreHunter.petInRange", "@ts.rabid" }},
+        --Fervor
+            { "82726", { "player.focus < 50", "pet.focus < 50" }},
+        --Kill Command
+            { "34026", { "@coreHunter.petInRange", "pet.exists" }},
+        --Kill Shot
+            { "53351", "target.health <= 20" },
+        --Rapid Fire T16 2piece
+            { "3045", { "player.spell(3045).cooldown = 0", "@ts.t16" }},
+        --Serpent Sting
+            { "1978", "!target.debuff(118253)" },
+        --Multishot
+            { "2643", { "!pet.buff(115939)", "player.focus >= 75", "!player.spell(34026).cooldown <= 2" }},
+        --Multishot
+            { "2643", { "!pet.buff(115939)", "player.focus >= 40", "player.spell(34026).cooldown >= 2" }},
+        --Glaive toss
+            { "117050" },
+        --Dire Beast
+            { "120679" },
+        --Focus Fire
+             { "82692", "@ts.focusfire" },
+        --Cobra Shot refresh Serpent Sting
+            { "77767", "target.debuff(118253).duration <= 3" },
+        --Arcane Shot
+            { "3044", { "player.focus >= 50", "player.spell(34026).cooldown >= 3" }},
+        --Arcane Shot
+            { "3044", "player.focus >= 70" },
+        --Cobra Shot
+            { "77767", { "@ts.casttime(77767)", "@ts.cobra(77767)", "!player.spell(34026).cooldown <= 1" }},
+        --Cobra Shot
+            { "77767", { "player.focus < 35", "player.spell(34026).cooldown >= 2" }},
+        }, "modifier.enemies = 2" },
+    }, "modifier.multitarget" },
+
+  --Single Target
+    --Bestial Wrath
+    { "19574", { "@coreHunter.petInRange", "pet.exists", "player.spell(34026).cooldown <= 2" }},
+    --Rabid Doesnt work
+    { "!/run CastSpellByID(53401)", { "@coreHunter.petInRange", "@ts.rabid" }},
+    --Fervor
+    { "82726", { "player.focus < 50", "pet.focus < 50" }},
+    --Kill Command
+    { "34026", { "@coreHunter.petInRange", "pet.exists" }},
+    --Kill Shot
+    { "53351", "target.health <= 20" },
+    --Rapid Fire T16 2piece
+    { "3045", { "player.spell(3045).cooldown = 0", "@ts.t16" }},
+    --Serpent Sting
+    { "1978", "!target.debuff(118253)" },
+    --Glaive Toss
+    { "117050", { "player.focus >= 55", "@ts.clip" }},
+    --Glaive Toss
+    { "117050", { "player.focus >= 40", "player.spell(34026).cooldown >= 2" }},
+    --Glaive Toss
+    { "117050", { "player.focus <= 40", "@ts.clip", "player.spell(34026).cooldown > 3" }},
+    --Dire Beast
+    { "120679" },
+    --Focus Fire
+    { "82692", "@ts.focusfire" },
+    --Cobra Shot refresh Serpent Sting
+    { "77767", "target.debuff(118253).duration <= 3" },
+    --Arcane Shot
+    { "3044", { "player.focus >= 50", "player.spell(34026).cooldown >= 3" }},
+    --Arcane Shot
+    { "3044", { "player.focus >= 70", "@ts.clip" }},
+    --Cobra Shot
+    { "77767", { "@ts.casttime(77767)", "@ts.cobra(77767)", "!player.spell(34026).cooldown <= 1" }},
+    --Cobra Shot
+    { "77767", { "player.focus < 35", "player.spell(34026).cooldown >= 2" }},
+    
+    
+
+},{
+  --Buffs
+    --Aspect of the Cheetah
+    { "5118", { "player.moving", "!player.buff(5118)", "!player.combat", "toggle.aspect" }},
+        
+  --Call Pet
+    --Pet 1
+    { "883", { "!pet.exists", "pet.alive" }},
+
+    --Mend Pet
+    { "136", { "pet.health <= 45", "!pet.buff(136)" }},
+        
+  --Binding Shot
+    { "109248", "modifier.ralt", "ground" },
+  --Wyvern Sting
+    { "19386", "modifier.lalt", "mouseover" },
+},
+
+function()
+ProbablyEngine.toggle.create(
+    'aspect',
+    'Interface\\Icons\\spell_hunter_aspectoftheironhawk',
+    'Auto Aspect',
+	'Enable/Disable auto aspect.')
+
+end)
